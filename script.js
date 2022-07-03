8b21b194220d065b5a728359381d58f73c9407f3
Ace <kronicrespawn@gmail.com>
2022-07-02 20:48:45 -0500

    added key display, css update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const para = document.getElementById("para");
 
 window.addEventListener("keyup", function(e) {
-    console.log(e.keyCode);
+    console.log(e);
+    buttonPress = e.key;
     number = e.keyCode;
     para.innerHTML = number;
+    para2.innerHTML = buttonPress;
 });
